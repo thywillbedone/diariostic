898c75d8ddacd723330e204449086eda4f273650
Damian Ferro <dfmartinez8909@gmail.com>
2022-01-31 11:22:15 +0100

    resumen semanal

--- secciones/portada.php
+++ secciones/portada.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $portada = [
 "titulo" => "La portada",
 "autor" => "mtorres",
-"resumen" => "Último minuto",
+"resumen" => "Resumen semanal",
 ];
 ?>
